1f74c73d5b842095f22b01952456cc8530a71386
Sourceduty <sourceduty@gmail.com>
2024-11-22 04:19:17 -0500

    Add files via upload

---
+++ Military_Program.py
@@ -0,0 +1,108 @@
+import time
+
+# Function to display the startup screen
+def display_startup_screen():
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> ARMY TERMINAL <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⣠⣀⡀⣀⣠⣤⣤⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣄⢠⣠⣼⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠀⠀⠀⢠⣤⣦⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⢦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣟⣾⣿⣽⣿⣿⣅⠈⠉⠻⣿⣿⣿⣿⣿⡿⠇⠀⠀⠀⠀⠀⠉⠀⠀⠀⠀⠀⢀⡶⠒⢉⡀⢠⣤⣶⣶⣿⣷⣆⣀⡀⠀⢲⣖⠒⠀⠀⠀⠀⠀⠀⠀")
+    print("⢀⣤⣾⣶⣦⣤⣤⣶⣿⣿⣿⣿⣿⣿⣽⡿⠻⣷⣀⠀⢻⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⣤⣶⣶⣤⣀⣀⣬⣷⣦⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣤⣦⣼⣀⠀")
+    print("⠈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠓⣿⣿⠟⠁⠘⣿⡟⠁⠀⠘⠛⠁⠀⠀⢠⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠏⠙⠁")
+    print("⠀⠸⠟⠋⠀⠈⠙⣿⣿⣿⣿⣿⣿⣷⣦⡄⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⣼⣆⢘⣿⣯⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡉⠉⢱⡿⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣟⡿⠦⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⡗⠀⠈⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣉⣿⡿⢿⢷⣾⣾⣿⣞⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⣠⠟⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣾⣿⣿⣷⣦⣶⣦⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠈⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠻⣿⣤⡖⠛⠶⠤⡀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠙⣿⣿⠿⢻⣿⣿⡿⠋⢩⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠧⣤⣦⣤⣄⡀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠘⣧⠀⠈⣹⡻⠇⢀⣿⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⣿⣿⣿⣤⣀⡀⠀⠀⠀⠀⠀⠀⠈⢽⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠹⣷⣴⣿⣷⢲⣦⣤⡀⢀⡀⠀⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⣿⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣷⢀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠂⠛⣆⣤⡜⣟⠋⠙⠂⠀⠀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⠉⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣾⣿⣿⣿⣿⣆⠀⠰⠄⠀⠉⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⠿⠿⣿⣿⣿⠇⠀⠀⢀⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡿⠛⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡇⠀⠀⢀⣼⠗⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠃⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠁⠀⠀⠀")
+    print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⠀⠀⠀⠀")
+    print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> INTERNATIONAL <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n")
+    print("ID: 439211 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 1984-6-14\n")
+
+# Function to display NATO phonetic alphabet
+def display_nato_phonetic_alphabet():
+    alphabet = {
+        'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot', 
+        'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliett', 'K': 'Kilo', 'L': 'Lima', 
+        'M': 'Mike', 'N': 'November', 'O': 'Oscar', 'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 
+        'S': 'Sierra', 'T': 'Tango', 'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 
+        'Y': 'Yankee', 'Z': 'Zulu'
+    }
+    print("\nNATO Phonetic Alphabet:\n")
+    for letter, code in alphabet.items():
+        print(f"{letter} ~ {code}")
+
+# Main menu with new subcommands
+def display_main_menu():
+    print("\nMain Menu:\n")
+    print("+----------------------------------+---------+------------------------+----------------+")
+    print("|             Sectors              |  Code   |         Short          |     Number     |")
+    print("+----------------------------------+---------+------------------------+----------------+")
+    sectors = [
+        ("Management", "G0001", "Tango-Five-One", "89.5-A"),
+        ("Radar", "G0002", "Tango-Four-Two", "1.43-B"),
+        ("Inventory", "G0003", "Tango-Eight-Two", "54.5-C"),
+        ("Missions", "G0004", "Tango-Two-Six", "63.1-D"),
+        ("Plans", "G0005", "Tango-Nine-Two", "5.54-E"),
+        ("Reroute", "G0006", "Tango-Seven-One", "43.1-F"),
+        ("Defense", "G0007", "Tango-Three-Zero", "22.4-G"),
+    ]
+    for sector in sectors:
+        print(f"| {sector[0]:<32} | {sector[1]:<7} | {sector[2]:<22} | {sector[3]:<14} |")
+    print("+----------------------------------+---------+------------------------+----------------+")
+
+# Subcommands for operations
+def operations_menu():
+    print("\nOperations Menu:")
+    print("[A] Operations - Mission Briefings, Strategy Planning")
+    print("[B] Communications - Secure Messaging, Radio Frequencies")
+    print("[C] Navigation - Map Overlays, Coordinates Entry")
+    print("[D] Logistics - Supply Inventory, Transport Requests")
+    print("[E] Intelligence - Recon Reports, Data Analysis")
+    print("[F] Emergency - Distress Signals, Medical Assistance")
+
+# Function to simulate terminal interaction
+def terminal_interface():
+    display_startup_screen()
+    display_main_menu()
+
+    while True:
+        print("\n=======================< COMMAND CENTER INTERFACE >=======================\n")
+        print("                             < DATE: 2023-11-25 >")
+        print("                     < OPERATIONAL STATUS: ACTIVE [GREEN] >\n")
+        
+        operation = input("Choose Operation: [A/B/C/D/E/F] or type 'HELP' for assistance\n>> ").upper()
+        
+        if operation == 'HELP':
+            operations_menu()
+        elif operation == 'A':
+            print("Operation A selected: Operations - Mission Briefings, Strategy Planning")
+        elif operation == 'B':
+            print("Operation B selected: Communications - Secure Messaging, Radio Frequencies")
+        elif operation == 'C':
+            print("Operation C selected: Navigation - Map Overlays, Coordinates Entry")
+        elif operation == 'D':
+            print("Operation D selected: Logistics - Supply Inventory, Transport Requests")
+        elif operation == 'E':
+            print("Operation E selected: Intelligence - Recon Reports, Data Analysis")
+        elif operation == 'F':
+            print("Operation F selected: Emergency - Distress Signals, Medical Assistance")
+        else:
+            print("Invalid selection. Type 'HELP' for available operations.")
+        
+        # Pause before displaying the terminal interface again
+        time.sleep(2)
+        
+        # Ask the user if they want to continue or exit
+        continue_option = input("\nDo you wish to continue? [Y/N]: ").upper()
+        if continue_option != 'Y':
+            print("Exiting the terminal interface.")
+            break
+
+# Run the terminal interface
+terminal_interface()
